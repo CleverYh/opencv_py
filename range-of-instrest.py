ec14aff665168aa08ad7e0bbc3f156a0c3b7526a
CleverYh <cleveryh@qq.com>
2020-04-02 17:01:47 +0800

    Update range-of-instrest.py

--- range-of-instrest.py
+++ range-of-instrest.py
@@ -3,19 +3,32 @@ from cv2 import cv2
 import numpy as np
 
 img = cv2.imread('a.jpg')
-cv2.namedWindow("Demo")
 
+cv2.namedWindow("Demo")
 roi = img[600:1200, 2100:3600]  # 1500x600 (r=600,c=1500)
 cv2.imshow("Demo", roi)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-cv2.namedWindow("Demo",0)
+
+cv2.namedWindow("Demo", 0)
 cv2.imshow("Demo", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+cv2.namedWindow("Demo", 0)
+b, g, r = cv2.split(img)
+cv2.imshow("Demo", b)
+cv2.waitKey(0)
+
+img = cv2.merge((r, g, b))
+cv2.imshow("Demo", img)
+cv2.waitKey(0)
+
+img = cv2.merge((b, g, r))
+cv2.imshow("Demo", img)
+cv2.waitKey(0)
+
 img[0:600, 1000:2500] = roi
-# b= img[:,:,0] # 截取蓝色通道
-cv2.namedWindow("Demo",0)
 cv2.imshow("Demo", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
