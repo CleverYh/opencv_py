2f0840bbe7eb9152501f18a63bec2f11c2d6e39e
CleverYh <cleveryh@qq.com>
2020-04-18 17:26:09 +0800

    Update 1.2.1-capture_by_cam.py

--- 1.2.1-capture_by_cam.py
+++ 1.2.1-capture_by_cam.py
@@ -2,17 +2,17 @@
 from cv2 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(700) # my cam ID is 700 on my notebook but it may not work on your computer. You can try to run "1.2.1.1-find-cam-id.py" to find your camera ID first.
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
