0eca01505b2f9edeafc9c2ea5a3002a0928ee961
CleverYh <cleveryh@qq.com>
2020-04-22 02:23:08 +0800

    Update 5.1-contours.py

--- 5.1-contours.py
+++ 5.1-contours.py
@@ -6,22 +6,26 @@ import matplotlib.pyplot as plt
 
 img = cv2.imread(r'pictures\cat.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-img_rgb = img
-img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-ret,thresh = cv2.threshold(img_gray,127,255,0)
+img_temp = cv2.imread(r'pictures\cat.jpg')
+img_rgb = cv2.cvtColor(img_temp, cv2.COLOR_BGR2RGB)
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(img_gray, 127, 255, 0)
 
 # There are three arguments in cv2.findContours() function, first one is source image, second is contour retrieval mode, third is contour approximation method. And it outputs the image, contours and hierarchy. contours is a Python list of all the contours in the image. Each individual contour is a Numpy array of (x,y) coordinates of boundary points of the object.
 # Note Since opencv 3.2 source image is not modified by this function.
 # image, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) #  not enough values to unpack (expected 3, got 2) in OpenCV 4, works in OpenCV 3
-contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) # works in OpenCV 4
+contours, hierarchy = cv2.findContours(
+    thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  # works in OpenCV 4
 
 # To draw the contours, cv2.drawContours function is used. It can also be used to draw any shape provided you have its boundary points. Its first argument is source image, second argument is the contours which should be passed as a Python list, third argument is index of contours (useful when drawing individual contour. To draw all contours, pass -1) and remaining arguments are color, thickness etc.
-cont_all = cv2.drawContours(img, contours, -1, (0,255,0), 3) # draw all the contours in an image
+# draw all the contours in an image
+cont_all = cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
 
-cont_4th = cv2.drawContours(img, contours, 3, (0,255,0), 3) # draw an individual contour, say 4th contour here
+# draw an individual contour, say 4th contour here
+cont_4th = cv2.drawContours(img, contours, 3, (0, 255, 0), 3)
 # the way below is the same as cont_4th but more useful
 cnt = contours[4]
-cont_4th_useful = cv2.drawContours(img, [cnt], 0, (0,255,0), 3)
+cont_4th_useful = cv2.drawContours(img, [cnt], 0, (0, 255, 0), 3)
 
 # Contours can be explained simply as a curve joining all the continuous points (along the boundary), having same color or intensity. The contours are a useful tool for shape analysis and object detection and recognition.
 
