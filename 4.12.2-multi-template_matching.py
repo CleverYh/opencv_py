0dddab4a47a04b9dff9f1e9cb63d5f8caec8d4c1
CleverYh <cleveryh@qq.com>
2020-07-13 17:32:25 +0800

    rename

--- 4.12.2-multi-template_matching.py
+++ 4.12.2-multi-template_matching.py
@@ -12,7 +12,7 @@ res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
 threshold = 0.8
 loc = np.where(res >= threshold)
 for pt in zip(*loc[::-1]):
-    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 1)
 
 # cv2.imwrite('res.png',img_rgb)
 plt.subplot(121), plt.imshow(img_gray, cmap='gray')
